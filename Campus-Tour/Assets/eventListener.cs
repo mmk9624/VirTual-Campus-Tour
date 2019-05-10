55a71d53c799eff6c09bc9e5b72c7a09f9940f2a
Carl Yao <mmk9624@vt.edu>
2019-05-10 19:00:31 -0400

    Initial project mirgration

---
+++ Campus-Tour/Assets/eventListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventListener : MonoBehaviour
+{
+    private Renderer _renderer;
+    public Material mat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    
+    }
+
+    public void OnEnter()
+    {
+        _renderer.material.color = Color.red;
+    }
+
+    public void OnExit()
+    {
+        _renderer.material = mat;
+    }
+
+    public void onGrab()
+    {
+
+    }
+}
